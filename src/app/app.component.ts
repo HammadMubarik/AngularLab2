57dbab3be3fa79ec59cfd550a6d12a2ac3d5827d
unknown <mubarikhammad@gmail.com>
2024-02-29 15:40:34 +0000

    Lab4

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,34 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-
+import { ListComponentComponent } from './list-component/list-component.component';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, ListComponentComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'data-binding-app';
+  myValue:boolean = true;
+  hide:boolean = true;
+
+  myCount:number = 0;
+  onSave()
+  {
+    alert('button pressed!')
+  }
+  onclick()
+  {
+    alert("h2 clicked")
+  }
+  count()
+  {
+    this.myCount++;
+    
+  }
+  onImgClick()
+  {
+    this.hide = !this.hide;
+  }
 }
